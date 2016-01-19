93322e024ce11452266424d6913917fb828542cf
worrytron <rastermancer@gmail.com>
2016-01-19 14:17:27 -0500

    working prototype

--- edlToLog.py
+++ edlToLog.py
@@ -1,5 +1,6 @@
 # built-in
 import sys
+import os
 import xml.etree.ElementTree as ET
 
 # external
@@ -10,55 +11,82 @@ from timecode import Timecode
 TC_BASE = '59.94'
 DEBUG = False
 
-QT_UI = "V:/dev/py/edlToLog.ui"
+class EdlList(QtGui.QListWidget):
+    def __init__(self, type, parent=None):
+        super(EdlList, self).__init__(parent)
+        self.setAcceptDrops(True)
 
-TEST_TREE = 'V:/dev/py/test_data/Sequence 02.xml'
-TEST_CSV = 'V:/dev/py/test_data/Sequence 02.csv'
-TEST_EDL = 'V:/dev/py/test_data/CFP ELEMENT REEL SALES.edl'
-EDL_CSV = 'V:/dev/py/test_data/CFP ELEMENT REEL SALES.csv'
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.accept()
+        else:
+            event.ignore()
+
+    def dragMoveEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.setDropAction(QtCore.Qt.CopyAction)
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.setDropAction(QtCore.Qt.CopyAction)
+            event.accept()
+            links = []
+            for url in event.mimeData().urls():
+                links.append(str(url.toLocalFile()))
+            self.emit(QtCore.SIGNAL("dropped"), links)
+        else:
+            event.ignore()
+
+    def iterate(self):
+        for i in range(self.count()):
+            yield self.item(i).text()
 
 
 class LogWidget(QtGui.QWidget):
 
     def __init__(self):
-        super(self.__class__, self).__init__()
-        
-        uic.loadUi(QT_UI, self)
+        super(LogWidget, self).__init__()
+
         self.setWindowTitle('Edit Log Generator')
+
+        self.list_view = EdlList(self)
+        self.exec_btn = QtGui.QPushButton('C O N V E R T')
+        self.exec_btn.setEnabled(0)
+
+        self.connect(self.list_view, QtCore.SIGNAL("dropped"), self.dropEDL)
         self.initUI()
 
     def initUI(self):
-        self.input_edl = ''
-        self.input_xml = ''
+        layout = QtGui.QVBoxLayout()
 
-        self.edl_btn.clicked.connect(self.importEDL)
-        #self.xml_btn.clicked.connect(self.importXML)
+        layout.addWidget(self.list_view)
+        layout.addWidget(self.exec_btn)
 
-        self.export_btn.setEnabled(False)
+        self.setLayout(layout)
+
+        self.exec_btn.clicked.connect(self.execute)
 
         self.show()
 
-    def importEDL(self, state):
-        self.input_edl = QtGui.QFileDialog.getOpenFileName()
+    def dropEDL(self, l):
+        for url in l:
+            if os.path.exists(url):
+                print(url)
+                self.exec_btn.setEnabled(1)
+                item = QtGui.QListWidgetItem(url, self.list_view)
+                item.setStatusTip(url)
 
-        if not self.input_edl == '':
-            self.export_btn.setEnabled(True)
-            self.xml_btn.setEnabled(False)
+    def execute(self, state):
+        for edl in self.list_view.iterate():
+            edl = str(edl)
+            if (".edl" in edl) or (".EDL" in edl):
+                edlToLog(edl)
+            elif (".xml" in edl) or (".XML" in edl):
+                xmlToLog(edl)
 
-        print self.input_edl
-
-    def importXML(self, state):
-        self.input_xml = QtGui.QFileDialog.getOpenFileName()
-        
-        if not self.input_xml == '':
-            self.export_btn.setEnabled(True)
-            self.xml_btn.setEnabled(False)
-
-        print self.input_edl
-
-    def export(self, state):
-        
-        print self.edl_btn.getEnabled()
 
 def run():
     app = QtGui.QApplication(sys.argv)
@@ -67,7 +95,6 @@ def run():
 
 
 def frameToTC(frame, base=TC_BASE):
-
     tc = Timecode(base, '00:00:00:00')
     tc.frames = frame
 
@@ -75,7 +102,6 @@ def frameToTC(frame, base=TC_BASE):
 
 
 def sequence(in_, out_, base=TC_BASE, debug=DEBUG):
-
     if isinstance(in_, int):
         in_ = frameToTC(in_)
 
@@ -91,8 +117,7 @@ def sequence(in_, out_, base=TC_BASE, debug=DEBUG):
     return (in_, out_)
 
 
-def xmlToLog(XML=TEST_TREE):
-
+def xmlToLog(XML, out_path=None):
     # LOAD XML TREE
     tree = ET.parse(XML)
     root = tree.getroot()
@@ -119,7 +144,7 @@ def xmlToLog(XML=TEST_TREE):
 
             # Add an extra linebreak if the clips do not butt together
             # (Detecting this is why we store the out frame on each iteration)
-            if in_ != xout:
+            if (in_ != xout):
                 out_string += ',,\n'
 
             # Write the line (note inline conversion from str to int to timecode)
@@ -129,15 +154,19 @@ def xmlToLog(XML=TEST_TREE):
             xout = out_
 
     # Write the CSV
-    with open(TEST_CSV, 'w') as out_stream:
+    if not (out_path):
+        out_path = os.path.dirname(XML)
+
+    out_csv = os.path.join(out_path, XML + '.csv')
+
+    with open(out_csv, 'w') as out_stream:
         out_stream.write(out_string)
 
 
-def edlToLog(EDL=TEST_EDL):
-
+def edlToLog(EDL, out_path=None):
     out_stream = ''
 
-    with open(TEST_EDL, 'r') as edl:
+    with open(EDL, 'r') as edl:
     
         for line in edl:
 
@@ -160,7 +189,14 @@ def edlToLog(EDL=TEST_EDL):
             '''
             out_stream += "{},{},{}\n".format(clip_name, clip_start, clip_end)
 
-    with open(EDL_CSV, 'w') as csv:
+
+    # Write the CSV
+    if not (out_path):
+        out_path = os.path.dirname(EDL)
+
+    out_csv = os.path.join(out_path, EDL + '.csv')
+
+    with open(out_csv, 'w') as csv:
         csv.write(out_stream)
 
     return True
