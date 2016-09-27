e706dc9cf8001b34a1c943b60951d25582cc4ae1
worrytron <mark.rohrer@gmail.com>
2016-09-27 09:00:10 -0400

    converts standard roll notation into ints

--- dice.py
+++ dice.py
@@ -1,15 +1,17 @@
 from random import choice
 import re
 
-dice_pattern = re.compile("(?P<numdice>\d*)(?P<d>d)(?P<die>\d*)(?P<bonus>\+|\-)(?P<amount>\d*)")
+dice_pattern = re.compile("(?P<numdice>\d*)(?P<d>d)(?P<die>\d*)(?P<op>\+|\-)(?P<bonus>\d*)")
 
 class Dice(object):
     SIDES = 2
-    def __init__(self, num_rolls=1):
+    def __init__(self, num_rolls=1, b=0):
         self._nr    = num_rolls
+    	self._bonus = b
         self._rolls = [0 for x in range(num_rolls)]
         self._sides = [i+1 for i in range(self.__class__.SIDES)]
         self.roll()
+        self.sum()
 
     def __getitem__(self, index):
         return self._rolls[index]
@@ -25,8 +27,11 @@ class Dice(object):
     def roll(self):
         for i in range(len(self._rolls)):
             self._rolls[i] = choice(self._sides)
+        
+    def sum(self):
+        _sum = sum(self._rolls) + self._bonus
+        self.sum = _sum if _sum > 1 else 1
         self.average = sum(self._rolls)/float(self._nr)
-        self.sum = sum(self._rolls)
 
 
 class d2(Dice):   SIDES = 2
@@ -40,19 +45,17 @@ class d20(Dice):  SIDES = 20
 class d100(Dice): SIDES = 100
 
 def roll(dice):
-    d = re.match(dice_pattern, dice)
-    die = int(d.group('die'))
-    num = int(d.group('numdice'))
-    bonus = int(d.group('amount'))
+    d     = re.match(dice_pattern, dice)
+    die   = int(d.group('die'))
+    num   = int(d.group('numdice'))
+    bonus = int(d.group('bonus'))
+    if (d.group('op') == '-'):
+    	bonus *= -1
 
     class ThisDie(Dice):
         SIDES = die
-    
-    d_roll = ThisDie(num)
+    d_roll = ThisDie(num, bonus)
 
-    if (d.group('bonus') == '+'):
-    	return d_roll.sum + bonus
-    else:
-    	return d_roll.sum - bonus
+    print d_roll.sum
 
-print roll('2d12+15')
+roll('2d12-20')
